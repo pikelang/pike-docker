7601c935fb3e4819fee78a022d6e1f3ff0364ed3
Bertrand Lupart <bertrand@gstack.io>
2021-11-18 20:52:06 +0100

    vars file for target-specific parameters

---
+++ build-push.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#set -x
+set -e
+
+bin_docker="/usr/local/bin/docker"
+
+# Docker hub
+user="bertrandlupart"
+repo="pike"
+
+
+# Build Dockerfile and push to Docker hub
+
+build_push()
+{
+  dir=$1
+
+  if [ ! -d "${dir}" ]; then
+    echo "${dir} is not a directory" >&2
+    return
+  fi
+
+  # Populate arch and tags
+  arch=""
+  tags=""
+  . "${dir}/vars"
+
+  vers=$(basename "${dir}")
+
+  # Get additional tags from vars file
+  more_tags=""
+  for t in ${tags}; do
+    more_tags="${more_tags} --tag=${user}/${repo}:${t}"
+  done
+
+  ${bin_docker} buildx build \
+    --tag="${user}/${repo}:${vers}" ${more_tags}\
+    --platform="${arch}" \
+    --push \
+    "${dir}"
+}
+
+
+# Either give directory/ies as argument(s), either loop over docker/ directory
+
+if [ $# == 0 ]; then
+  for elem in "docker"/*; do
+    echo "building ${elem}"
+    build_push "${elem}"
+  done
+else
+  for elem in $*; do
+    echo "building ${elem}"
+    build_push "${elem}"
+  done
+fi
